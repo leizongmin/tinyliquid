80058120eafe80567d0877da27fdd45e8088f4f4
Lei Zongmin <leizongmin@gmail.com>
2012-03-28 17:18:06 +0800

    f

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-module.exports = require('./lib/template');
+// ±àÒëº¯Êı
+exports.compile = require('./lib/template').compile;
+
+// äÖÈ¾º¯Êı
+exports.render = require('./lib/template').render;
+
+// ¹ıÂËÆ÷
+exports.filters = require('./lib/filters');
+
