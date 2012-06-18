5515991a013afcdc1422329cf44fe3c5a8536c01
Lei Zongmin <leizongmin@gmail.com>
2012-06-18 15:53:19 +0800

    变量支持 a.[b]  b.[c].[e].f 之类的索引

--- test/utils_localsWrap.js
+++ test/utils_localsWrap.js
@@ -23,4 +23,25 @@ describe('utils.split()', function () {
     utils.localsWrap('abc.efg.').should.equal('"abc.efg."');
   });
   
+  it('#variable index', function () {
+    utils.localsWrap('abc.[cde]').should.equal('locals.abc[locals.cde]');
+    utils.localsWrap('abc.[cde].fg').should.equal('locals.abc[locals.cde].fg');
+    utils.localsWrap('abc.[cde].[fg]').should.equal('locals.abc[locals.cde][locals.fg]');
+    utils.localsWrap('abc.[cde.[hi.j].k].[fg]')
+      .should.equal('locals.abc[locals.cde[locals.hi.j].k][locals.fg]');
+    utils.localsWrap('a.[b.[c.[e].[f.[g]].[h]].h.[i.[j]].k].l.[m].n')
+      .should.equal('locals.a[locals.b[locals.c[locals.e][locals.f[locals.g]][locals.h]].h[locals.i[locals.j]].k].l[locals.m].n');
+  });
+  
+  it('#analysis', function () {
+  
+    var v = [];
+    utils.localsWrap('abc.efg.hi', undefined, function (a) { v.push(a) });
+    v.should.eql(['abc.efg.hi']);
+    
+    var v = [];
+    utils.localsWrap('a.[b.[c.[e].[f.[g]].[h]].h.[i.[j]].k].l.[m].n', undefined, function (a) { v.push(a) });
+    v.should.eql(['e', 'g', 'h', 'j', 'm']);
+  });
+  
 });
