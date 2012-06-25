6666dfc260a913fde166ccf02e94dd7d7a2b4e5c
Lei Zongmin <leizongmin@gmail.com>
2012-06-25 16:29:56 +0800

    修正解析case/when时出错问题

--- test/tags_case.js
+++ test/tags_case.js
@@ -6,7 +6,7 @@ describe('Liquid.js', function () {
   it('#case', function () {
   
     var render = function (text, data, filters) {
-      //console.log(liquid.parse(text));
+      //console.log(liquid.parse(text).code);
       var fn = liquid.compile(text);
       var html = fn(data, filters);
       return html;
@@ -29,7 +29,19 @@ describe('Liquid.js', function () {
       {link_to_vendor: function (vendor) {
         return '<a>' + vendor + '</a>';
       }}).should.equal('OKOK');
+      
+    render('{% case template %}\n{% when \'index\' %}Welcome{% when \'product\' %}{{ product.vendor | link_to_vendor }} / {{ product.title }}{% else %}{{ page_title }}{% endcase %}',
+      {page_title: 'OKOK'},
+      {link_to_vendor: function (vendor) {
+        return '<a>' + vendor + '</a>';
+      }}).should.equal('OKOK');
+      
+    render('{% case a %}\n'
+         + '{% when 1 %}{% if b == 1 %}c{%else%}d{%endif%}\n'
+         + '{% when 2 %}a\n'
+         + '{% endcase %}', {a: 1, b:1})
+      .should.equal('c\n');
         
-    });
+  });
   
 });
