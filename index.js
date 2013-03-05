53b169ef59a8dea3ee506f3fd5d63e0a71b46c82
Lei Zongmin <leizongmin@gmail.com>
2013-03-05 12:59:20 +0800

    封装tinyliquid.run()

--- index.js
+++ index.js
@@ -19,7 +19,24 @@ exports.parse = function () {
 
 // 执行中间代码
 var vm = require('./lib/vm');
-exports.run = vm.run;
+exports.run = function () {
+  if (arguments.length < 1) throw new Error('Not enough arguments.');
+  var originCallback = arguments[arguments.length - 1];
+  var hasCallback = false;
+  var callback = function (err) {
+    // 保证回调函数只执行一次
+    if (hasCallback) return;
+    hasCallback = true;
+    originCallback.apply(null, arguments);
+  };
+  arguments[arguments.length - 1] = callback;
+  // 如果抛出异常，以回调方式返回
+  try {
+    vm.run.apply(null, arguments);
+  } catch (err) {
+    callback(err);
+  }
+};
 
 
 // 执行中间件代码时的环境对象
