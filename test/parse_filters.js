65348f4fa0fd0f0a020dc15d216958462e857a55
Zongmin Lei <leizongmin@gmail.com>
2014-03-18 18:04:43 +0800

    Fixed: 解析filters时，名称和冒号后面有空格导致解析异常

---
+++ test/parse_filters.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+var common = require('./common');
+
+
+describe('Parse filters', function () {
+
+  var context = common.newContext();
+
+  it('#normal', function (done) {
+    common.taskList()
+      .add(function (done) {
+        common.render(context, '{{"a"|append:" b, c"}}', function (err, buf) {
+          assert.equal(err, null);
+          // console.log(buf);
+          assert.equal(buf, 'a b, c');
+          context.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        var tpl = '{{"a"|append:" | b | "|append:"c"}}';
+        common.render(context, tpl, function (err, buf) {
+          assert.equal(err, null);
+          // console.log(buf);
+          assert.equal(buf, 'a | b | c');
+          context.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        var tpl = '{{"a"|append: " | b | "| append:"c"}}';
+        common.render(context, tpl, function (err, buf) {
+          assert.equal(err, null);
+          console.log(buf);
+          assert.equal(buf, 'a | b | c');
+          context.clearBuffer();
+          done();
+        });
+      })
+      .end(done);
+  });
+
+});
