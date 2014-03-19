baf69a9651741b1b3215d7f2441ca3be4ad4865f
Zongmin Lei <leizongmin@gmail.com>
2014-03-19 12:00:44 +0800

    Fixed: utils.getChildValue() when value=null throws error

---
+++ test/utils.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var utils = require('../lib/utils');
+
+describe('utils', function () {
+
+  it('#getChildValue', function () {
+    var data = {
+      a: 123,
+      b: {
+        c: 456,
+        d: 789
+      },
+      e: null,
+      f: false
+    };
+    assert.deepEqual(utils.getChildValue(data, ['a']), [true, 123]);
+    assert.deepEqual(utils.getChildValue(data, ['b', 'c']), [true, 456]);
+    assert.deepEqual(utils.getChildValue(data, ['c']), [false, null]);
+    assert.deepEqual(utils.getChildValue(data, ['e']), [true, null]);
+    assert.deepEqual(utils.getChildValue(data, ['e', 'f']), [false, null]);
+    assert.deepEqual(utils.getChildValue(data, ['e', 'f', 'g']), [false, null]);
+    assert.deepEqual(utils.getChildValue(data, ['f']), [true, false]);
+    assert.deepEqual(utils.getChildValue(data, ['f', 'g']), [false, null]);
+    assert.deepEqual(utils.getChildValue(data, ['f', 'g', 'h']), [false, null]);
+  });
+
+});
