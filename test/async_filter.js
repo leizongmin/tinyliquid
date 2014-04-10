993f8bb6f379efe97f06153e72a01d9fd3b307f2
Zongmin Lei <leizongmin@gmail.com>
2014-04-10 12:25:40 +0800

    issue #19 support async filter cache

--- test/async_filter.js
+++ test/async_filter.js
@@ -3,7 +3,7 @@ var common = require('./common');
 
 
 describe('Async: filter', function () {
-  
+
   var context = common.newContext();
   context.setFilter('a', function (v) {
     return 'sync:' + v;
@@ -47,6 +47,146 @@ describe('Async: filter', function () {
           assert.equal(err instanceof Error, true);
           done();
         });
+      })
+      .end(done);
+  });
+
+  it('#cache - 1', function (done) {
+    var ctx = common.newContext();
+    ctx.from(context);
+    var c1 = 0;
+    var c2 = 0;
+    function filter1 (a, callback) {
+      c1++;
+      callback(null, a + c1);
+    }
+    filter1.enableCache = true;
+    function filter2 (a, callback) {
+      c2++;
+      callback(null, a + c2);
+    }
+    ctx.setAsyncFilter('filter1', filter1);
+    ctx.setAsyncFilter('filter2', filter2);
+    common.taskList()
+      .add(function (done) {
+        common.render(ctx, '{{123|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err, buf);
+          assert.equal(err, null);
+          assert.equal(buf, '124-124');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{123|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err, buf);
+          assert.equal(err, null);
+          assert.equal(buf, '124-125');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{123|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err);
+          assert.equal(err, null);
+          assert.equal(buf, '124-126');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{789|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err);
+          assert.equal(err, null);
+          assert.equal(buf, '791-127');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{789|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err);
+          assert.equal(err, null);
+          assert.equal(buf, '791-128');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        assert.equal(c1, 2);
+        assert.equal(c2, 5);
+        done();
+      })
+      .end(done);
+  });
+
+  it('#cache - 2', function (done) {
+    var ctx = common.newContext();
+    ctx.from(context);
+    var c1 = 0;
+    var c2 = 0;
+    function filter1 (a, callback) {
+      c1++;
+      callback(null, a + c1);
+    }
+    filter1.enableCache = true;
+    function filter2 (a, callback) {
+      c2++;
+      callback(null, a + c2);
+    }
+    ctx.setAsyncFilter('filter1', filter1);
+    ctx.setAsyncFilter('filter2', filter2);
+    common.taskList()
+      .add(function (done) {
+        common.render(ctx, '{{123|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err, buf);
+          assert.equal(err, null);
+          assert.equal(buf, '124-124');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{123|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err, buf);
+          assert.equal(err, null);
+          assert.equal(buf, '124-125');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{123|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err);
+          assert.equal(err, null);
+          assert.equal(buf, '124-126');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{789|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err);
+          assert.equal(err, null);
+          assert.equal(buf, '791-127');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        common.render(ctx, '{{789|filter1}}-{{123|filter2}}', function (err, buf) {
+          //console.log(err);
+          assert.equal(err, null);
+          assert.equal(buf, '791-128');
+          ctx.clearBuffer();
+          done();
+        });
+      })
+      .add(function (done) {
+        assert.equal(c1, 2);
+        assert.equal(c2, 5);
+        done();
       })
       .end(done);
   });
